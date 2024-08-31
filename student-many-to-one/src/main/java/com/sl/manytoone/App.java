98f603f74dfc2171b2d940d95a5c9a278587a007
Silvio Limeira <sicemal@gmail.com>
2024-08-31 20:41:14 -0300

    Many To Many

---
+++ student-many-to-one/src/main/java/com/sl/manytoone/App.java
@@ -0,0 +1,43 @@
+package com.sl.manytoone;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+
+	private static final Logger logger = LogManager.getLogger(App.class);
+
+	public static <T> void save(T[] entity, EntityManager em) {
+		em.getTransaction().begin();
+		for (T t : entity) {
+			em.persist(t);
+		}
+		em.getTransaction().commit();
+	}
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("com.sl.manytoone");
+		EntityManager entityManager = factory.createEntityManager();
+
+		University university = new University("MIT");
+		
+		Student s1 = new Student();
+		s1.setStudentName("Kevin");
+		s1.setUniversity(university);
+		
+		Student s2 = new Student();
+		s2.setStudentName("Joe");
+		s2.setUniversity(university);
+		
+		save(new Object[] {university, s1, s2}, entityManager);
+		
+		entityManager.close();
+		factory.close();
+
+	}
+}
