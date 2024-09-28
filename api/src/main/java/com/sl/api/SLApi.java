797fa9794b701fdd171f8f96bb8298b10a887ea8
Silvio Limeira <sicemal@gmail.com>
2024-09-28 10:20:32 -0300

    java sdk version, add api and front modules

---
+++ api/src/main/java/com/sl/api/SLApi.java
@@ -0,0 +1,13 @@
+package com.sl.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SLApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SLApi.class, args);
+	}
+
+}
---
+++ front/src/main/java/com/sl/front/SLFront.java
@@ -0,0 +1,13 @@
+package com.sl.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SLFront {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SLFront.class, args);
+	}
+
+}
