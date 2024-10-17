5159b821a7c4f4cf41e1a62cf26f6d854ebaa08c
Silvio Limeira <sicemal@gmail.com>
2024-10-17 20:30:58 -0300

    add swagger openapi

--- api-admin/src/main/java/com/sl/api/admin/controller/TreeController.java
+++ api-admin/src/main/java/com/sl/api/admin/controller/TreeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +21,14 @@ import com.sl.api.admin.repository.TreeRepository;
 import com.sl.api.admin.service.AppService;
 import com.sl.api.admin.service.TreeServiceImpl;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
-@RequestMapping("v1/trees")
+@RequestMapping(value = "v1/trees", produces = {"aplication/json"})
+@Tag(name = "trees")
 public class TreeController {
 
 	private static final Logger logger = LogManager.getLogger(TreeController.class);
@@ -32,15 +39,29 @@ public class TreeController {
 		this.treeService = treeService;
 	}
 
-	@GetMapping
+	@GetMapping(produces = "application/json")
+	@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Tree register successfully")})
 	public ResponseEntity<List<TreeDTO>> findAll() {
 		return ResponseEntity.ok(treeService.findAll());
 	}
 
+	@Operation(summary = "Save Trees", method = "POST")
+	@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Tree register successfully")})
 	@CrossOrigin(origins = "http://localhost:8090")
-	@PostMapping
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<TreeDTO> create(@RequestBody TreeDTO dto) {
 		return ResponseEntity.ok(treeService.save(dto));
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
