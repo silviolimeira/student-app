5fb12a006ac2eac82dfc33259ddb743810152858
Silvio Limeira <sicemal@gmail.com>
2024-10-26 15:27:51 -0300

    add @Transcational annotation

--- api-admin/src/main/java/com/sl/api/admin/service/TreeServiceImpl.java
+++ api-admin/src/main/java/com/sl/api/admin/service/TreeServiceImpl.java
@@ -6,12 +6,14 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sl.api.admin.entity.Tree;
 import com.sl.api.admin.model.TreeDTO;
 import com.sl.api.admin.repository.TreeRepository;
 
 @Service
+@Transactional(readOnly = true)
 public class TreeServiceImpl {
 
 	private static final Logger logger = LogManager.getLogger(TreeServiceImpl.class);
@@ -22,6 +24,7 @@ public class TreeServiceImpl {
 		this.treeRepository = treeRepository;
 	}
 	
+	@Transactional
 	public TreeDTO save(TreeDTO dto) {
 		Tree tree = new Tree(dto);
 		logger.info("save={}", dto);
