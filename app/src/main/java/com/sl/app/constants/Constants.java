ecb7fc9a6256704dea34466a126e424ba2f95f6c
Silvio Limeira <sicemal@gmail.com>
2024-10-27 11:22:07 -0300

    add constants

---
+++ app/src/main/java/com/sl/app/constants/Constants.java
@@ -0,0 +1,11 @@
+package com.sl.app.constants;
+
+public class Constants {
+
+    private Constants() {
+
+    }
+
+    public static final String LOGO_URL = "images/tech_logo.png";
+    public static final String LOGIN_URL = "images/login_icon.png";
+}
